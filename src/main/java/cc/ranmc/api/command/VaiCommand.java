0b0f95ab3c2c64483a0f1d3ef0af2654c93e78df
Ran <xyfwdy@gmail.com>
2026-03-09 21:28:43 +0800

    新增 村民ai恢复指令

---
+++ src/main/java/cc/ranmc/api/command/VaiCommand.java
@@ -0,0 +1,66 @@
+package cc.ranmc.api.command;
+
+import cc.ranmc.api.Main;
+import cc.ranmc.api.util.BasicUtil;
+import com.bekvon.bukkit.residence.api.ResidenceApi;
+import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static cc.ranmc.api.util.BasicUtil.color;
+
+public class VaiCommand implements CommandExecutor {
+    
+    private final static Main plugin = Main.getInstance();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender,
+                             @NonNull Command cmd,
+                             @NotNull String label,
+                             String @NonNull [] args) {
+
+
+        if (!sender.hasPermission("roa.admin")) {
+            sender.sendMessage(color("&a没有权限"));
+            return true;
+        }
+
+        // 以下指令不能在控制台输入
+        if (!(sender instanceof Player player)) {
+            BasicUtil.print(color("&c该指令不能在控制台输入"));
+            return true;
+        }
+
+        Bukkit.getWorlds().forEach(world -> {
+            for (Entity entity : world.getEntities()) {
+                entity.getScheduler().run(Main.getInstance(), task -> {
+                    if (entity instanceof Villager villager && !villager.hasAI()) {
+                        villager.setAI(true);
+                    }
+                }, ()->{});
+            }
+        });
+        sender.sendMessage(color("&a创建成功,快打开宣传栏查看吧"));
+
+        return true;
+    }
+}
